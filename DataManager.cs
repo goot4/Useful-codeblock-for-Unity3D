c6fffb43760d950ab15f91d3d929d20beef7bc6a
goot4 <55455296+goot4@users.noreply.github.com>
2021-11-08 17:28:38 +0800

    Update DataManager.cs

--- DataManager.cs
+++ DataManager.cs
@@ -1,5 +1,7 @@
 /* A standard example of saving and loading Data using JSON file.
- * 
+ * Dataflow: AddNew -> SaveData -> SaveDataFormat -> JSON -> SaveDataFormat -> GetSaveData
+ * Use SaveDataFormat to increase Savedata protection level.
+ * And use SaveDataFormat can simplify the structure, save part of the data.
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -10,12 +12,11 @@ public static class DataManager
 {
     [SerializeField] private static int RankNum = 10;
 
-    [System.Serializable]
     public class Score
     {
         // Must be public to be write to JSON.
-        public string userName;
-        public int score;
+        public string userName { get; private set; }
+        public int score { get; private set; }
 
         public Score(string newUserName, int newScore)
         {
@@ -27,7 +28,6 @@ public static class DataManager
             return score >= comparedScore;
         }
     }
-    [System.Serializable]
     private class SaveData
     {
         public List<Score> scores = new List<Score>();
@@ -65,18 +65,50 @@ public static class DataManager
         List<Score> scores = _saveData.scores;
         return scores;
     }
+
+
+    // Interact with JSON file.
+    // Cannot define ScoreFormat in SaveDataFormat.
+    [System.Serializable]
+    public class ScoreFormat
+    {
+        public string userName;
+        public int score;
+        public ScoreFormat(string newUserName, int newScore)
+        {
+            userName = newUserName;
+            score = newScore;
+        }
+    }
+    [System.Serializable]
+    private class SaveDataFormat
+    {
+        public List<ScoreFormat> scoresFormat= new List<ScoreFormat>();
+    }
     public static void SaveToJSON()  // Save data between sessions (to JSON)
     {
-        string json = JsonUtility.ToJson(_saveData);
+        SaveDataFormat saveDataFormat = new SaveDataFormat();
+        foreach(Score score in _saveData.scores)
+        {
+            saveDataFormat.scoresFormat.Add(new ScoreFormat(score.userName, score.score));
+        }
+        string json = JsonUtility.ToJson(saveDataFormat);
+
         File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
     public static void LoadFromJSON()
     {
         string path = Application.persistentDataPath + "/savefile.json";
+
         if (File.Exists(path))
         {
             string json = File.ReadAllText(path);
-            _saveData = JsonUtility.FromJson<SaveData>(json);
+            SaveDataFormat saveDataFormat = new SaveDataFormat();
+            saveDataFormat = JsonUtility.FromJson<SaveDataFormat>(json);
+            foreach(ScoreFormat scoreFormat in saveDataFormat.scoresFormat)
+            {
+                _saveData.scores.Add(new Score(scoreFormat.userName, scoreFormat.score));
+            }
         }
     }
 }
